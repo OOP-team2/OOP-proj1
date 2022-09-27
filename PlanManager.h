36f94730201bc212ba264e0d20d633fb8e925ae7
hojooneum <bldolphin96@gmail.com>
2022-09-27 23:48:33 +0900

    fix PlanManager class like other classes

--- PlanManager.h
+++ PlanManager.h
@@ -8,26 +8,21 @@
 #ifndef OOP_PROJ1_PLANMANAGER_H
 #define OOP_PROJ1_PLANMANAGER_H
 
-namespace planmanager {
 
-    class PlanManager {
-    private:
-        PlanManager(std::vector<Plan> newPlans);
+class PlanManager {
+private:
+    PlanManager(std::vector<Plan> newPlans);
+    std::vector<Plan> plans;
+public:
+    PlanManager();
+    explicit PlanManager(const std::vector <Plan> &plans);
+    void addPlan(Plan plan);
+    void deletePlan(Plan plan);
+    void reviseMeal(Plan plan, Meal meal);
+    void reviseMealType(Plan plan, Meal_Type meal_type);
+    void reviseDate(Plan plan, Date date);
+    void showAllPlan();
+};
 
-        std::vector<Plan> plans;
-    public:
-        PlanManager();
-
-        explicit PlanManager(const std::vector <Plan> &plans);
-
-        void addPlan(Plan plan);
-        void deletePlan(Plan plan);
-        void reviseMeal(Plan plan, Meal meal);
-        void reviseMealType(Plan plan, Meal_Type meal_type);
-        void reviseDate(Plan plan, Date date);
-        void showAllPlan();
-    };
-
-} // planmanager
 
 #endif //OOP_PROJ1_PLANMANAGER_H
