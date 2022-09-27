cbd1e0b42c21e598173fce3b1627328f52a08da8
hojooneum <bldolphin96@gmail.com>
2022-09-27 23:37:23 +0900

    fix Date class

--- Date.h
+++ Date.h
@@ -5,9 +5,25 @@
 #ifndef OOP_PROJ1_DATE_H
 #define OOP_PROJ1_DATE_H
 
+#include "string"
 
 class Date {
-
+private:
+    int year;
+    int month;
+    int day;
+    string comment;
+public:
+    Date();
+    Date(int new_year, int new_month, int new_day, string new_comment);
+    int getYear();
+    void setYear(int new_year);
+    int getMonth();
+    void setMonth(int new_month);
+    int getDay();
+    void setDay(int new_day);
+    string getComment();
+    void setComment(string new_comment);
 };
 
 
