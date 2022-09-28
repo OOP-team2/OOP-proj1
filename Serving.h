d7327a3b22f08af9fa0493bf933ef04f6d10e398
hojooneum <bldolphin96@gmail.com>
2022-09-28 16:36:24 +0900

    add Serving class

---
+++ Serving.h
@@ -0,0 +1,19 @@
+//
+// Created by HoJoonEum on 2022/09/28.
+//
+
+#ifndef OOP_PROJ1_SERVING_H
+#define OOP_PROJ1_SERVING_H
+
+
+class Serving {
+private:
+    string name;
+public:
+    Serving(string serving_name);
+    string getName();
+    void setName(string new_name);
+};
+
+
+#endif //OOP_PROJ1_SERVING_H
