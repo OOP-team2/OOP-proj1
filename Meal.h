b5060d01379d35cbe1d772d830662847ab4540f6
hojooneum <bldolphin96@gmail.com>
2022-09-28 16:26:31 +0900

    add Meal class

--- Meal.h
+++ Meal.h
@@ -6,11 +6,16 @@
 #define OOP_PROJ1_MEAL_H
 
 
-
 class Meal {
-
+private:
+    vector<Serving> servings;
+public:
+    Meal();
+    void addServing(Serving serving, int people);
+    void deleteServing(Serving serving_to_delete);
+    void showServings();
+    vector<Serving> getServings();
 };
-
 
 
 #endif //OOP_PROJ1_MEAL_H
