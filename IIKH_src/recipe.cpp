45e63c3317a1d378ff98043e1d7cbb77d6f49302
featuresname <ksu9124@naver.com>
2022-10-04 11:08:33 +0900

    add id

--- IIKH_src/recipe.cpp
+++ IIKH_src/recipe.cpp
@@ -1,18 +1,22 @@
 #include "recipe.h"
 
-Recipe::Recipe(std::string new_recipe_name,int new_time,std::vector<Ingredient> new_ingredients, std::vector<std::string> new_cooking_order){
+Recipe::Recipe(int new_ID,std::string new_recipe_name,int new_prepare_time,std::vector<Ingredient> new_ingredients, std::vector<std::string> new_cooking_order){
+    ID = new_ID;
     recipe_name = new_recipe_name;
-    time = new_time;
+    prepare_time = new_prepare_time;
     ingredients = new_ingredients;
     cooking_order = new_cooking_order;
 }
 
 //getdata
+int getID(){
+    return ID;
+}
 std::string Recipe::getRecipeName(){
     return recipe_name;
 }
-int Recipe::getTime(){
-    return time;
+int Recipe::getPrepareTime(){
+    return prepare_time;
 }
 std::vector<Ingredient> Recipe::getIngredients(){
     return ingredients;
@@ -26,8 +30,8 @@ std::vector<std::string> Recipe::getCookingOrder(){
 void Recipe::setRecipeName(std::string name) {
     recipe_name = name;
 }
-void Recipe::setTime(int s_time) {
-    time = s_time;
+void Recipe::setPrepareTime(int s_time) {
+    prepare_time = s_time;
 }
 void Recipe::setIngredients(std::vector<Ingredient> s_ingredients) {
     ingredients = s_ingredients;
@@ -41,7 +45,7 @@ void Recipe::setCookingOrder(std::vector<std::string> order){
 void Recipe::editIngredient(Ingredient edit_ingredient){
     for (int i=0; i<ingredients.size(); i++) {
         if(ingredients[i].getName()==edit_ingredient.getName()) {
-            if(ingredients[i].getWeight() + edit_ingredient.getWeight()>0){
+            if((ingredients[i].getWeight() + edit_ingredient.getWeight())>0){
                 ingredients[i].setWeight(ingredients[i].getWeight() + edit_ingredient.getWeight());
                 return;
             }
@@ -76,7 +80,7 @@ void Recipe::deleteCookingOrder(){
 //수정 요망
 void Recipe::printRecipe(){
     std::cout << " Recipe Name: " << getRecipeName() << std::endl;
-    std::cout << " Cooking Time: " << getTime() << std::endl;
+    std::cout << " Cooking Time: " << getPrepareTime() << std::endl;
 
     if (getIngredients().size())
     {
