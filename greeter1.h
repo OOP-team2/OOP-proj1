4c890cb59cf1466b42a78d2265f90a5468388261
xylitolboy <101115515+xylitolboy@users.noreply.github.com>
2022-10-05 18:35:22 +0900

    Update greeter1.h

--- greeter1.h
+++ greeter1.h
@@ -3,7 +3,6 @@
 #include "InputManager.h"
 #include <memory>
 
-
 class Greeter {
 private:
 	unique_ptr<RecipeDatabase> recipedb;
@@ -14,16 +13,17 @@ public:
 	Greeter();
 	void showTitle();
 	void showMenu();
-	void addRecipe(); // add new recipe
-	void deleteRecipe(); // delete recipe
-	void updateRecipe(); // edit recipe
-	void searchRecipe(); // search recipe by ingredients or recipename
-	void showAllRecipe(); // show all recipe
-	void addPlan(Plan* plan_to_add);
-	void addPlan(int year, int month, int day, int meal_type, Plan plan_to_add); // function overloading
-	void deletePlan();
-	void revisePlan();
-	void searchPlan();
-	void showPlan();
-	void showPeriod();
+	void insertRecipe();
+	void deleteRecipe();
+	void updateRecipe();
+	void showAllRecipe();
+	vector<Recipe> getRecipes();
+	vector<Recipe> searchRecipesByIngredient();
+	vector<Recipe> searchRecipesByRecipeName();
+	void addPlan();
+    void deletePlan();
+    void reviseMeal();
+    void reviseMealType();
+    void reviseDate();
+    void showAllPlan();
 };
