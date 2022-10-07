5f80273225fe23b656bc53029c5b7f982c5b243c
hojooneum <bldolphin96@gmail.com>
2022-10-07 23:44:25 +0900

    add comment field agatin to Date class

--- IIKH_src/Date.h
+++ IIKH_src/Date.h
@@ -11,15 +11,18 @@ private:
     int year;
     int month;
     int day;
+    std::string annotation;
 public:
     Date();
-    Date(int new_year, int new_month, int new_day);
+    Date(int new_year, int new_month, int new_day, std::string new_annotation);
     int getYear();
     void setYear(int new_year);
     int getMonth();
     void setMonth(int new_month);
     int getDay();
     void setDay(int new_day);
+    std::string getAnnotation();
+    void setAnnotation(std::string new_annotation);
     void showInfo();
 
     bool operator== (Date other_date);
