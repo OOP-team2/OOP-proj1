c1a266b0dd84ff2573d9548ab69e88b5ea99e99d
hojooneum <bldolphin96@gmail.com>
2022-10-07 23:56:25 +0900

    add FileManagerForPlans

--- IIKH_src/FileManager.h
+++ IIKH_src/FileManager.h
@@ -1,7 +1,7 @@
 #ifndef OOP_PROJ1_FILEMANAGER_H
 #define OOP_PROJ1_FILEMANAGER_H
 
-#include <string>
+#include "string"
 #include "vector"
 
 class FileManager{
@@ -10,9 +10,20 @@ class FileManager{
     
     public:
         FileManager();
-        FileManager(std::string file_name);
+        FileManager(std::string new_file_name);
         std::vector<std::vector< std::string> > loadRecipeDB();
         void writeRecipeDB(std::vector<std::vector<std::string>> data);
 };
+
+class FileManagerForPlans {
+private:
+    std::string file_name;
+public:
+    FileManagerForPlans();
+    FileManagerForPlans(std::string file_name);
+    std::vector<std::vector< std::string> > loadPlans();
+    void writePlans(std::vector<std::vector<std::string>> plandata);
+};
+
 
 #endif
