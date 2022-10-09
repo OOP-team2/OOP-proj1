838ca7aa0609151e659b84f41f62f8810f8dc024
hojooneum <bldolphin96@gmail.com>
2022-10-10 00:48:16 +0900

    add comment on main function

--- IIKH_src/main.cpp
+++ IIKH_src/main.cpp
@@ -1,9 +1,8 @@
-//
-// Created by HoJoonEum on 2022/09/26.
-//
 #include "iostream"
 #include "greeter.h"
 using namespace std;
+
+// main function. entry point of this program
 int main() {
     Greeter g = Greeter();
     g.showMenu();
