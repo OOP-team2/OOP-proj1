f4a70a62acb90c14fbe3d8e5a44422b0096fe02d
hojooneum <bldolphin96@gmail.com>
2022-10-10 02:18:16 +0900

    add comments to Meal

--- IIKH_src/Meal.cpp
+++ IIKH_src/Meal.cpp
@@ -1,41 +1,46 @@
-//
-// Created by HoJoonEum on 2022/09/28.
-//
-
 #include "Meal.h"
 
-
+// initialize a Meal instance
 Meal::Meal() {};
+// initialize mealtype and people
 Meal::Meal(MealType new_mealtype, std::string new_people) {
     mealtype = new_mealtype;
     people = new_people;
 };
 
+// returns mealtype
 MealType Meal::getMealType() {
     return mealtype;
 }
 
+// set new value to mealtype
 void Meal::setMealType(MealType new_mealtype) {
     mealtype = new_mealtype;
 }
 
+// returns people
 std::string Meal::getPeople() {
     return people;
 }
+
+// set new value to people
 void Meal::setPeople(std::string new_people) {
     people = new_people;
 }
 
+// pushes new recipe back to recipes
 void Meal::addRecipe(Recipe recipe) {
     recipes.push_back(recipe);
     std::cout << "a recipe is added" << std::endl;
     return;
 }
 
+// set new value to recipes
 void Meal::setRecipes(std::vector<Recipe> ex_recipes) {
     recipes = ex_recipes;
 }
 
+// delete a recipe from recipes
 void Meal::deleteRecipe(Recipe recipe_to_delete) {
     auto iter = recipes.begin();
     for (auto recipe : recipes) {
@@ -51,14 +56,20 @@ void Meal::deleteRecipe(Recipe recipe_to_delete) {
     return;
 }
 
+// returns recipes
 std::vector<Recipe> Meal::getRecipes() {
     return recipes;
 }
 
+// print out all recipe info in recipes
 void Meal::showRecipes() {
+    for (auto recipe : recipes) {
+        recipe.showInfo();
+    }
     return;
 }
 
+// examine other meal to check if same meal
 bool Meal::isSame(Meal other_meal) {
     if (people == other_meal.getPeople() && mealtype == other_meal.getMealType()) {
         return true;
@@ -68,7 +79,8 @@ bool Meal::isSame(Meal other_meal) {
     }
 }
 
-bool Meal::isLesser(Meal other_meal) {
+// examine other meal to check if less meal
+bool Meal::isLess(Meal other_meal) {
     if (mealtype < other_meal.getMealType()) return true;
     if (mealtype > other_meal.getMealType()) return false;
     if (people < other_meal.getPeople()) return true;
@@ -76,6 +88,7 @@ bool Meal::isLesser(Meal other_meal) {
     else return false;
 }
 
+// recives a stringfied meal type then returns a MealType
 MealType Meal::stringfiedToMealType(std::string string_meal_type) {
     if (string_meal_type == "Breakfast") {
         return Breakfast;
@@ -91,6 +104,7 @@ MealType Meal::stringfiedToMealType(std::string string_meal_type) {
     }
 }
 
+// recives a MealType then returns stringfied MealType
 std::string Meal::mealTypeToString(MealType meal_type) {
     std::string stringfied = "";
     switch (meal_type)
